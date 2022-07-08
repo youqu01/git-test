3b2ccd7a40433ae69ada2862f0217fb1d3ae381c
youqu <123@qq.com>
2022-07-08 16:28:07 +0800

    master commit2

--- src/main/java/com/youqu/git/GitTest.java
+++ src/main/java/com/youqu/git/GitTest.java
@@ -6,6 +6,6 @@ public class GitTest {
         System.out.println("hello world2");
         System.out.println("hello world2");
         System.out.println("hello hot-fix1");
-        System.out.println("hello hot-fix2");
+        System.out.println("hello master");
     }
 }
