feec9df057da0dbfcd26bf07db8157405ee8cad5
youqu <123@qq.com>
2022-07-08 16:07:14 +0800

    hot-fix commit1

--- src/main/java/com/youqu/git/GitTest.java
+++ src/main/java/com/youqu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello world");
         System.out.println("hello world2");
         System.out.println("hello world2");
+        System.out.println("hello hot-fix1");
     }
 }
