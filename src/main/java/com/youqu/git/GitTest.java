d3694724358805f000fe3c93c9524a509f8b5a05
youqu <123@qq.com>
2022-07-08 15:54:56 +0800

    two commit

--- src/main/java/com/youqu/git/GitTest.java
+++ src/main/java/com/youqu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.youqu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world2");
     }
 }
