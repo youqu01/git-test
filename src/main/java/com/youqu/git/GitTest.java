73eab37665fdd36f71d875afbf69a362d5fd0edb
youqu01 <108910477+youqu01@users.noreply.github.com>
2022-07-08 16:53:41 +0800

    youqu01

--- src/main/java/com/youqu/git/GitTest.java
+++ src/main/java/com/youqu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello master");
         System.out.println("hello hot-fix2");
         System.out.println("push");
+        System.out.println("pull");
     }
 }
